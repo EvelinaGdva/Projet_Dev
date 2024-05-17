14934143b7fc1e8011ef856195ee401d886f4b0b
mehita fanny <mehita.fanny@ynov.com>
2024-05-17 10:39:18 +0200

    menu-front recup

--- menu-front/menu.php
+++ menu-front/menu.php
@@ -1,7 +1,4 @@
-<?php 
-    include('../Data/database.php'); 
-    include('login-check.php');
-?>
+<?php include('Data/database.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,8 +6,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Website</title>
-    <link rel="stylesheet" href="Css/index.css">
+
+    <link rel="stylesheet" href="css/index.css">
 </head>
+
 <body>
     <section class="navbar">
         <div class="container">
@@ -22,28 +21,21 @@
 
             <div class="menu text-right">
                 <ul>
-                    <li><a href="<?php echo SITEURL; ?>index.php">Home</a></li>
-                    <li><a href="<?php echo SITEURL; ?>category.php">Category</a></li>
-                    <li><a href="<?php echo SITEURL; ?>food.php">Food</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>category.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>food.php">Foods</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
                 </ul>
             </div>
 
             <div class="clearfix"></div>
         </div>
     </section>
-
-    <div class="menu text-center">
-        <div class="wrapper">
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="adminController.php">Admin</a></li>
-                <li><a href="categoryController.php">Category</a></li>
-                <li><a href="foodController.php">Food</a></li>
-                <li><a href="orderController.php">Order</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-        </div>
-    </div>
-</body>
-</html>
